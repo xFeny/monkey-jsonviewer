efeb1be2cac6f4dff9f7c0f9f9326925b5533d81
Feny <1220301855@qq.com>
2024-10-14 23:03:48 +0800

    URL https

--- src/common/URL.js
+++ src/common/URL.js
@@ -1,7 +1,7 @@
 export default {
-  LAYUI_JS: "//unpkg.com/layui@2.7.6/dist/layui.js",
-  LAYUI_CSS: "//unpkg.com/layui@2.7.6/dist/css/layui.css",
-  EXAMPLE_JSON: "//fetch-api.feny.ink/example.json",
-  ONLINE_HTTP_REQUEST: "//fetch-api.feny.ink/httpRequest",
-  JSON_CRACK_WIDGET: "//jsoncrack.feny.ink/widget",
+  JSON_CRACK_WIDGET: "https://jsoncrack.feny.ink/widget",
+  EXAMPLE_JSON: "https://fetch-api.feny.ink/example.json",
+  LAYUI_JS: "https://unpkg.com/layui@2.7.6/dist/layui.js",
+  LAYUI_CSS: "https://unpkg.com/layui@2.7.6/dist/css/layui.css",
+  ONLINE_HTTP_REQUEST: "https://fetch-api.feny.ink/httpRequest",
 };
