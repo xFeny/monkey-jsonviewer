1ea9b10dfa7aac80752a06c51581ee70f4e9c9dc
Feny <1220301855@qq.com>
2024-11-27 13:06:12 +0800

    返回顶部样式修改

--- src/scrollTop/index.js
+++ src/scrollTop/index.js
@@ -1,8 +1,10 @@
+import tippy from "tippy.js";
 import Utils from "../common/Utils";
-import "./scroll-top.css";
+import "./scroll-top.scss";
 
-const scroll = Utils.createElement("div", { titile: "返回顶部", class: "scroll-top" });
+const scroll = Utils.createElement("div", { class: "scroll-top" });
 document.body.appendChild(scroll);
+tippy(scroll, { theme: "scroll", placement: "left", content: "返回顶部" });
 const $container = Utils.query(".container");
 $container.addEventListener("scroll", function () {
   this.scrollTop > 500 ? Utils.show(scroll) : Utils.hide(scroll);
